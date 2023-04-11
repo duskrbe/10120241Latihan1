eb7fced5f6cfcfe116099eeaa06f7664b2bd119d
Arby Febrian Saputro <oginkacida2@gmail.com>
2023-04-12 01:34:15 +0700

    menambah/mengubah/menghapus komponen tugas (finishing)

--- app/src/main/java/edu/arbyfebrian/a10120241latihan1/MainActivity.kt
+++ app/src/main/java/edu/arbyfebrian/a10120241latihan1/MainActivity.kt
@@ -3,6 +3,10 @@ package edu.arbyfebrian.a10120241latihan1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//12 April 2023
+//10120241
+//Arby Febrian Saputro
+//IF-6
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
